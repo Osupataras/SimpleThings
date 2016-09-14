64d198492359da28e743bd9593d58a33c5dbbfe9
Taras Osypchuk <osupa@ukr.net>
2016-09-14 16:24:20 +0300

    Add mouse move actions;Add point apple for eating and some effort fir eat;start to add applemove;

--- src/SimpleGame/Constans.java
+++ src/SimpleGame/Constans.java
@@ -8,5 +8,7 @@ public interface Constans {
     public int WINDOW_HEIGHT=300;
     public Color color=Color.BLUE;
     public int increment=1;
+    public int direction_x=0;
+    public int direction_y=0;
 
 }
--- src/mouse/MouseListernerEngine.java
+++ src/mouse/MouseListernerEngine.java
@@ -15,9 +15,12 @@ public class MouseListernerEngine implements MouseListener,MouseMotionListener, 
     Game mouse;
     Random random=new Random();
 
+
     public MouseListernerEngine(Game listener){
         mouse=listener;
+
     }
+
     public void eat(){
         if (mouse.point.x>mouse.apple.x-mouse.pointsizex && mouse.point.x<mouse.apple.x+5
                 && mouse.point.y>mouse.apple.y-mouse.pointsizey && mouse.point.y<mouse.apple.y+5){
@@ -25,14 +28,19 @@ public class MouseListernerEngine implements MouseListener,MouseMotionListener, 
             mouse.pointsizey+=5;
             mouse.apple.x=random.nextInt(WINDOW_WIDTH);
             mouse.apple.y=random.nextInt(WINDOW_HEIGHT);
+            direction_x=random.nextInt(6)-3;
+            direction_y=random.nextInt(6)-3;
         }
 
     }
 
-    public void applemove(){
-        int movedirection=random.nextInt(3);
 
-    }
+    public void applemove(){
+        mouse.apple.x+=direction_x;
+        mouse.apple.y+=direction_y;
+
+
+        }
 
 
     public void mouseClicked(MouseEvent e) {
@@ -72,19 +80,25 @@ public class MouseListernerEngine implements MouseListener,MouseMotionListener, 
         if (mouse_x>mouse.point.x && mouse_y>mouse.point.y){
             mouse.point.x+=increment;
             mouse.point.y+=increment;
+            applemove();
         }
         else if(mouse_x>mouse.point.x && mouse_y<mouse.point.y){
             mouse.point.x+=increment;
             mouse.point.y-=increment;
+            applemove();
         }
         else if(mouse_x<mouse.point.x && mouse_y>mouse.point.y){
             mouse.point.x-=increment;
             mouse.point.y+=increment;
+            applemove();
         }
         else if(mouse_x<mouse.point.x && mouse_y<mouse.point.y){
             mouse.point.x-=increment;
             mouse.point.y-=increment;
+            applemove();
         }
+
+
         eat();
         mouse.repaint();
 
